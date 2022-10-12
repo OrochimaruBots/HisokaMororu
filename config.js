ab926314cd88fca3251e68ea3482260096da8625
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-10-12 16:49:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6288804947282']
+global.premium = ['6288804947282']
+global.packname = 'GojoBotzMD'
 global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.sessionName = 'sessiob'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
